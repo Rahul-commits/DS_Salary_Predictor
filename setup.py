a33bed1807348658cc75a7cf02c2d123a96edc34
Rahul-commits <rahul.narramaneni@gmail.com>
2023-07-07 20:42:09 -0400

    Requirements and setup added

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="DS_Salary_predictor",
+    version='0.0.1',
+    author='Rahul',
+    author_email='rahul.narramaneni@gmail.com',
+    packages=find_packages(),
+    install_requirements=get_requirements('requirements.txt')
+)
